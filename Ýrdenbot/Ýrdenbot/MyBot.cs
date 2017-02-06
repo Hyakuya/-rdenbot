00246bea94b6c7b03228f5c6327c2ca8bcfd5532
oversoul96 <limpan.naslund@hotmail.com>
2017-02-06 20:39:34 +0100

    core

---
+++ Ýrdenbot/Ýrdenbot/MyBot.cs
@@ -0,0 +1,68 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÝrdesBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+        
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '§';
+                x.AllowMentionPrefix = true;
+
+
+            });
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("hi")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Anaria shola");
+                });
+            commands.CreateCommand("admin")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hyakuya and Darkmatter");
+                });
+            commands.CreateCommand("bye")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Al diel shala");
+                });
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Bal'a dash, malanore");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("Mjc4MjQyMDQ3MzE0MzYyMzY4.C3pcnQ.I2AlEOXPOymTIeDNgcPYgzX9YWE", TokenType.Bot);
+                
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+
+}
